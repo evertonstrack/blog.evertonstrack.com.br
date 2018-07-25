a51918535005894d20213a301af59ba632571f44
evertonstrack <everton.strack@gmail.com>
2018-07-25 01:00:59 -0300

    Mobile adjustments

--- gulpfile.js
+++ gulpfile.js
@@ -11,9 +11,16 @@ gulp.task('css', () => {
     .pipe(gulp.dest('assets'));
 });
 
+gulp.task('css:watch', () => {
+  gulp.src(cssFiles)
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(concat('all.css'))
+    .pipe(gulp.dest('./_site/assets'));
+});
+
 
 gulp.task('watch', () => {
-  gulp.watch(cssFiles, ['css']);
+  gulp.watch(cssFiles, ['css:watch']);
 });
 
 gulp.task('default', ['css']);
