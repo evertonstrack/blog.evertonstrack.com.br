45cb102c6f70130b57f5e0aa0789bfe1e906436a
Everton Strack <everton.strack@gmail.com>
2021-04-02 16:09:26 -0300

    Test serve always from cache and update cache when request its done (#111)
    * Test serve always from cache and update cache when request its done
    
    * Ajustado para cachear 27 posts

--- app/service-worker.js
+++ app/service-worker.js
@@ -8,7 +8,7 @@ const filesToCache = [
   {% for page in site.pages_to_cache %}
 '{{ page }}',
   {% endfor %}
-{% for post in site.posts limit: 12 %}
+{% for post in site.posts limit: 27 %}
 '{{ post.url }}',
   {% endfor %}
 {% for asset in site.files_to_cache %}
